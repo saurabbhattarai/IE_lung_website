8a777092b83bb029b140e9b06313e17295de4ac0
Dipesh Thapa <dthapa4682@gmail.com>
2026-03-19 12:27:21 +0545

    add doctor's data

---
+++ lib/constants/doctorsData.ts
@@ -0,0 +1,58 @@
+export const providers = [
+  {
+    id: 1,
+    name: "Dr. Paresh C. Giri",
+    speciality: "Pulmonary & Critical Care Medicine",
+    credentials: "MD",
+    fullDescription:
+      "Dr. Paresh Giri is board certified in pulmonary and critical care medicine and has a special focus on pulmonary hypertension and advanced lung disease. In addition to caring for patients at Inland Empire Lung, he teaches future doctors and pulmonologists as an Adjunct Associate Professor at Loma Linda University School of Medicine. He established a nationally recognized pulmonary hypertension program at Loma Linda University Medical Center to improve access to expert care in the Inland Empire and he is part of the national committee that reviews accreditation for centers of excellence in Pulmonary Hypertension. Dr. Giri believes in shared decision-making, taking time to explain conditions clearly and helping patients choose treatments that fit their goals and values. His approach blends clinical excellence with empathy and respect.",
+    image: "./doctors_images/dr_paresh_giri.webp",
+  },
+  {
+    id: 2,
+    name: "Dr. Roger Seheult",
+    speciality: "Sleep Medicine & Medical Education Specialist",
+    credentials: "MD",
+    fullDescription:
+      "Dr. Seheult is currently an Associate Clinical Professor at the University of California, Riverside School of Medicine, and an Assistant Clinical Professor at the School of Medicine and Allied Health at Loma Linda University. In 2012 he and Kyle Allred founded MedCram L.L.C., a medical education company with CME-accredited videos that are utilized by hospitals, medical schools, and hundreds of thousands of medical professionals from all over the world (and over 1 million YouTube Subscribers). His passion is promoting healthy lifestyles and regularly lectures to schools, hospitals, and media outlets. Dr. Seheult was the recipient of the 2021 San Bernardino County Medical Society's William L. Cover MD Award for Outstanding Contribution to Medicine and the 2022 United Health Group's The Sages of Clinical Service Award. In 2022, both Roger Seheult and Kyle Allred received the HRH Prince Salman bin Hamad Al Khalifa Medical Merit Medal from the Kingdom of Bahrain for their contribution to health policy in the Kingdom of Bahrain. His passion is demystifying medical concepts. For media inquiries related to Dr. Seheult and MedCram, please visit https://www.medcram.com/pages/media-contact.",
+    image: "./doctors_images/dr_roger_sehenult.webp",
+  },
+
+  {
+    id: 3,
+    name: "Dr. Luke Buxton",
+    speciality: "Pulmonary, Critical Care & Sleep Medicine Specialist",
+    credentials: "DO",
+    fullDescription:
+      "Luke Buxton, DO, is a pulmonologist and critical care physician who completed his Pulmonary and Critical Care fellowship at Arrowhead Regional Medical Center (2021–2024), where he served as Chief Fellow, and subsequently completed a Sleep Medicine Fellowship at Loma Linda University Medical Center (2024–2025). He previously served as Chief Resident during his Internal Medicine residency at Franciscan Health Olympia Fields and worked as a full-time nocturnist managing high-volume admissions and open ICU coverage. Dr. Buxton earned his Doctor of Osteopathic Medicine degree from the Chicago College of Osteopathic Medicine and holds a Bachelor of Science in Health Sciences from UC Santa Cruz. His academic contributions include peer-reviewed publications focused on pulmonary and COVID-19–related outcomes, as well as case reports in respiratory disease. He has received recognition for leadership and excellence in training, including ACP Resident of the Year, and maintains active interests in sleep medicine, wilderness medicine, and community service.",
+    image: "./doctors_images/dr_luke_buxton.webp",
+  },
+  {
+    id: 4,
+    name: "Dr. Tarik Ngab",
+    speciality: "Pulmonary & Critical Care Specialist (Advanced Bronchoscopy)",
+    credentials: "MD",
+    fullDescription:
+      "Dr. Tarik Ngab is a board-certified pulmonary and critical care physician with over 10 years of experience caring for patients with complex lung and critical illnesses. He treats conditions such as asthma, COPD, interstitial lung disease, and respiratory failure, and also cares for critically ill patients in the intensive care unit. Dr. Ngab serves as Director of Cardiopulmonary Services and Director of Critical Care Services at Redlands Community Hospital, where he works to improve patient care and safety. He has special expertise in advanced lung procedures, including robotic bronchoscopy, and in managing complex cardiopulmonary conditions. Dr. Ngab is also passionate about teaching and serves as an Assistant Clinical Professor at UC Riverside, helping train future physicians while providing compassionate, patient-centered care.",
+    image: "./doctors_images/dr_tarik_ngab.webp",
+  },
+  {
+    id: 5,
+    name: "Dr. Sahib Grewal",
+    speciality:
+      "Pulmonary & Critical Care Specialist (Advanced Bronchoscopy & Interventional Pulmonology)",
+    credentials: "MD",
+    fullDescription:
+      "Dr. Sahib Grewal is a Pulmonary and Critical Care physician who is passionate about helping patients with complex lung diseases and serious medical conditions. His clinical interests include pulmonary embolism, pulmonary hypertension, interstitial lung disease, sepsis, and advanced bronchoscopic procedures such as robotic bronchoscopy and endobronchial ultrasound. He is also experienced in the use of advanced hemodynamic devices to care for critically ill patients.",
+    image: "./doctors_images/dr_sahib_grewal.webp",
+  },
+  {
+    id: 6,
+    name: "Dr Enrique Gil",
+    speciality: "Pulmonary, Critical Care & Sleep Medicine Specialist",
+    credentials: "MD",
+    fullDescription:
+      "Dr. Gil received his medical training at several leading institutions, beginning with Family Practice training at Riverside University Health System and an Internal Medicine residency at Loma Linda University. He then completed a Pulmonary and Critical Care fellowship at UCLA Medical Center. He also trained in Sleep Medicine under Dr. Phil Westbrook, a founding member of the American Academy of Sleep Medicine. Dr. Gil served for several years as Medical Director of the Intensive Care Unit at Redlands Community Hospital and was also Chief of Staff. He is dedicated to serving the Redlands community by providing compassionate, expert care to every patient.",
+    image: "./doctors_images/dr_enrique_gil.webp",
+  },
+];
