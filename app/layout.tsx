5f36a94b179de15d1f1b5dcd4efdd7a2d7d7b99a
Dipesh Thapa <dthapa4682@gmail.com>
2026-05-11 10:23:13 +0545

    add google tag mananger script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import BackToTop from "@/components/BackToTop";
 import "./globals.css";
+import Script from "next/script";
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
@@ -16,7 +17,23 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <meta
+        name="google-site-verification"
+        content="znVWUmrqFacnlDsbMNaejDd0RlGrpjeYTMtWboYmhDA"
+      />
       <body className="font-sans antialiased bg-background text-foreground">
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4KYPP4V2C8"
+        ></Script>
+        <Script>
+          {` window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-4KYPP4V2C8');`}
+        </Script>
         {children}
         <BackToTop />
       </body>
